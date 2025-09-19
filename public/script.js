b3efde40c97b7ea01240f133fd5fcdb826590dc9
tinymog <hkmplusnow@gmail.com>
2025-09-19 08:32:00 -0300

    att19/09

--- public/script.js
+++ public/script.js
@@ -1,11 +1,9 @@
 let chave = "ca69a224d3ad72ef1702053b15d13bcf";
-//ca69a224d3ad72ef1702053b15d13bcf
 
 const cliquenobotao = () => {
-      let cidade = document.querySelector(".input-cidade").value;
-    
+    let cidade = document.querySelector(".input-cidade").value;
 
-    if(!cidade){
+    if (!cidade) {
         alert("Digite uma Cidade");
         return;
     }
@@ -14,26 +12,29 @@ const cliquenobotao = () => {
 }
 
 async function buscarCidade(cidade) {
-    try{
+    try {
         const resposta = await fetch(`api/previsao?cidade=${cidade}`);
         const dados = await resposta.json();
 
-        if(resposta.status !== 200){
-            alert("Erro" + (dados.erro || "Cidade não encontrada"));
-            return
+        if (resposta.status !== 200) {
+            alert("Erro: " + (dados.erro || "Cidade não encontrada"));
+            return;
         }
-    }catch(erro){
-        alert("Erro ao conectar ao servidor");
-        console.erro(erro);
-    }
 
         colocarNaTela(dados);
+
+    } catch (erro) {
+        alert("Erro ao conectar ao servidor");
+        console.error(erro);
+    }
 }
 
-function colocarNaTela(dados){
+function colocarNaTela(dados) {
     document.querySelector(".nome-cidade").innerHTML = dados.name;
     document.querySelector(".graus-cidade").innerHTML = dados.main.temp;
     document.querySelector(".cidade-desc").innerHTML = dados.weather[0].description;
     document.querySelector(".umidade-cidade").innerHTML = dados.main.humidity;
-    document.querySelector(".icon").innerHTML = dados.weather[0].icon;
+    const iconCode = dados.weather[0].icon;
+    const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+    document.querySelector(".icon").innerHTML = `<img src="${iconUrl}" alt="Ícone do clima">`;
 }
